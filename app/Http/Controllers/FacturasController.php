759bd021eff30ff7e56e47d62355e930f25f888a
ivan alejandro sambrana <=>
2020-12-16 17:32:22 -0300

    Borro modelo facuta

--- app/Http/Controllers/FacturasController.php
+++ app/Http/Controllers/FacturasController.php
@@ -10,13 +10,13 @@ class FacturasController extends Controller
 
     public function facturacion()
     {
-        $conexion = Auth::user()->medidor->conexion;
-        // $histolectura = Auth::user()->medidor->lecturas->last();
-        $medidor = Auth::user()->medidor;
-        $persona = Auth::user()->persona;
-          $factura = Auth::user()->medidor->conexion->facturas->last()->paginate(10);
-        //  $factura = 1;
-        // dd($factura);
-        return view('facturacion',compact("conexion","medidor","persona","factura"));
+        // $conexion = Auth::user()->medidor->conexion;
+        // // $histolectura = Auth::user()->medidor->lecturas->last();
+        // $medidor = Auth::user()->medidor;
+        // $persona = Auth::user()->persona;
+        //   $factura = Auth::user()->medidor->conexion->facturas->last()->paginate(10);
+        // //  $factura = 1;
+        // // dd($factura);
+        // return view('facturacion',compact("conexion","medidor","persona","factura"));
     }
 }
