e60f15dd3908d44a025a0286e19582e87741b646
aflorenciacabrera <aflorenciacabrera@gmail.com>
2020-12-16 17:39:24 -0300

    .

--- app/Http/Controllers/FacturasController.php
+++ app/Http/Controllers/FacturasController.php
@@ -10,7 +10,6 @@ class FacturasController extends Controller
 
     public function facturacion()
     {
-<<<<<<< HEAD
         $conexion = Auth::user()->medidor->conexion;
         // $histolectura = Auth::user()->medidor->lecturas->last();
         $medidor = Auth::user()->medidor;
@@ -20,15 +19,5 @@ class FacturasController extends Controller
         //  $factura = 1;
         // dd($factura);
         return view('facturacion',compact("conexion","medidor","persona","factura", "suministro"));
-=======
-        // $conexion = Auth::user()->medidor->conexion;
-        // // $histolectura = Auth::user()->medidor->lecturas->last();
-        // $medidor = Auth::user()->medidor;
-        // $persona = Auth::user()->persona;
-        //   $factura = Auth::user()->medidor->conexion->facturas->last()->paginate(10);
-        // //  $factura = 1;
-        // // dd($factura);
-        // return view('facturacion',compact("conexion","medidor","persona","factura"));
->>>>>>> main
     }
 }
