d2a5af52ff5d7acfd90ace1ee2777d15428f080d
aflorenciacabrera <aflorenciacabrera@gmail.com>
2020-12-19 12:44:53 -0300

    pagina de error

--- resources/views/vendor/laravelpwa/offline.blade.php
+++ resources/views/vendor/laravelpwa/offline.blade.php
@@ -1,36 +1,43 @@
+{{-- ------------------------------------------------ --}}
 @extends('layouts.app')
 @section('content')
+
 <div class="container">
     <div class="row justify-content-center ">
-        <div class="col-md-6 ">
-            <div class="card  ">              
+        <div class="col-md-8 ">
+            <div class="card  " style=" filter: drop-shadow(2px 4px 6px black);">              
                 <div class="card-body"> 
-                        <div class=" text-center  ">
-                            <p><h1 class="text-bold "> <i class="fa fa-exclamation-triangle" aria-hidden="true"></i> </h1></p>
-                                <p><h1 class="text-bold "> Lo sentimos!!</h1></p>
-                                <small><h3 class="text-bold ">Por favor verifique su conexión a internet</h3></small>
-                            {{-- <p><small>  <h3>Por favor verifique su conexión a internet</h3></small></p> --}}
-                            {{-- <a href="{{route('/')}}" class="btn btn-verde btn-lg">Volver a inicio</a>      --}}
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class=" text-center  ">
+                                {{-- <p><h1 class="text-bold "> <i class="fa fa-exclamation-triangle" aria-hidden="true"></i> </h1></p> --}}
+                            <img class="img img-responsive w-60"  src="{{asset('image/conexion.jpg')}}" alt="">
+                            </div>
+                        </div>
+                        <div class="col-md-8">
+                        <div class=" text-center py-md-3 ">
+                            <h1 class="text-bold "><strong>Sin conexión a Internet<strong></h1>
+                            <h4 class="text-bold ">Por favor verifique su conexión <br> Wi-Fi o de datos </h4>
+                            {{-- <p><small>Es posible que el enlace esté dañado o que se haya eliminado la página </small></p> --}}
+                            <a href="{{ url('/') }}" class="btn btn-info btn-lg text-white" role="button">Volver a intentar</a>  
                         </div>                                          
-                        {{-- <div class=" text-right  ">
-                            <img class="img img-responsive w-60"  src="{{asset('images/error-404.png')}}" alt="">
-                        </div> --}}
+                    </div>    
+                    </div> 
                 </div>
             </div>
         </div>       
     </div>
 </div>
-
-@endsection 
+@endsection
 <style>
     .pull-left {
     float: left;
     position: absolute;
     }
     .w-60 {
-    width: 60% !important;
-}
-h1, .h1 {
-    font-size: 3.25rem !important;
-}
+    width: 100% !important;
+    }
+    /* h1, .h1 {
+        font-size: 3.25rem !important;
+    } */
 </style>
