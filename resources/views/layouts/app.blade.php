fccbf6b7b33f0c44ce5f9c2b133a22bc121a1ccf
ivan alejandro sambrana <=>
2020-12-19 22:08:46 -0300

    quito swal del ready

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -284,33 +284,33 @@
     // $('.prueba').on('click',function(){
     console.log("click");
     let timerInterval
-    Swal.fire({
-      title: 'Cargando',
-      html: '',
-    //   timer: 2000,
-    background: '#f3f6f9e6 ',
-      timerProgressBar: true,
-      didOpen: () => {
-        Swal.showLoading()
-        timerInterval = setInterval(() => {
-          const content = Swal.getContent()
-          if (content) {
-            const b = content.querySelector('b')
-            if (b) {
-              b.textContent = Swal.getTimerLeft()
-            }
-          }
-        }, 100)
-      },
-      willClose: () => {
-        clearInterval(timerInterval)
-      }
-    }).then((result) => {
-      /* Read more about handling dismissals below */
-      if (result.dismiss === Swal.DismissReason.timer) {
-        console.log('')
-      }
-    })
+    // Swal.fire({
+    //   title: 'Cargando',
+    //   html: '',
+    // //   timer: 2000,
+    // background: '#f3f6f9e6 ',
+    //   timerProgressBar: true,
+    //   didOpen: () => {
+    //     Swal.showLoading()
+    //     timerInterval = setInterval(() => {
+    //       const content = Swal.getContent()
+    //       if (content) {
+    //         const b = content.querySelector('b')
+    //         if (b) {
+    //           b.textContent = Swal.getTimerLeft()
+    //         }
+    //       }
+    //     }, 100)
+    //   },
+    //   willClose: () => {
+    //     clearInterval(timerInterval)
+    //   }
+    // }).then((result) => {
+    //   /* Read more about handling dismissals below */
+    //   if (result.dismiss === Swal.DismissReason.timer) {
+    //     console.log('')
+    //   }
+    // })
     
     //   })
     $(document).ready(function(){        
