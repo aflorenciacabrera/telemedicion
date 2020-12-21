d542a272f49646b2930b5d6b4ab520db0deee3e1
aflorenciacabrera <aflorenciacabrera@gmail.com>
2020-12-21 08:52:22 -0300

    cargando y correccion de fecha factura

--- public/js/sweetalert2.js
+++ public/js/sweetalert2.js
@@ -1,10 +1,41 @@
+// ************************ cargando en cada pagina *********************************
+let timerInterval
+Swal.fire({
+  title: 'Cargando',
+  html: '',
+//   timer: 2000,
+background: '#f3f6f9e6 ',
+  timerProgressBar: true,
+  didOpen: () => {
+    Swal.showLoading()
+    timerInterval = setInterval(() => {
+      const content = Swal.getContent()
+      if (content) {
+        const b = content.querySelector('b')
+        if (b) {
+          b.textContent = Swal.getTimerLeft()
+        }
+      }
+    }, 100)
+  },
+  willClose: () => {
+    clearInterval(timerInterval)
+  }
+}).then((result) => {
+  /* Read more about handling dismissals below */
+  if (result.dismiss === Swal.DismissReason.timer) {
+    console.log('')
+  }
+});
+
+// *******************cargando en cada pagina ***************************
 $('.cargando').on('click',function(){
     console.log("click");
     let timerInterval
 Swal.fire({
   title: 'Cargando',
   html: '',
-  timer: 2000,
+  // timer: 2000,
   timerProgressBar: true,
   didOpen: () => {
     Swal.showLoading()
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -75,7 +75,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-2">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary cargando">
                                     {{-- {{ __('Login') }} --}}
                                     Consultar
                                 </button>
