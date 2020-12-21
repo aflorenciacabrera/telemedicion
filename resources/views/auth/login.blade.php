65768b8ff22cdce6a0add0fc7ebe164943315b52
ivan alejandro sambrana <=>
2020-12-21 13:41:12 -0300

    login a prod

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,7 +11,7 @@
                 </div>
 
                 <div class="card-body text-center">
-                    <form method="POST" action="{{ route('medidor.login') }}">
+                    <form  id="form" method="POST" action="{{ route('medidor.login') }}">
                         @csrf
 
                         <div class="form-group row">
